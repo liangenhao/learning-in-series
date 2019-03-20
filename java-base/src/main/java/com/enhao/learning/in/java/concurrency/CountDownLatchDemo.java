d5cf2c13c8febfc6b203b43404dbe75af99c2dd7
enhao <liangenhao@hotmail.com>
2019-03-20 14:39:08 +0800

    Add 并发工具类案例

---
+++ java-base/src/main/java/com/enhao/learning/in/java/concurrency/CountDownLatchDemo.java
@@ -0,0 +1,36 @@
+package com.enhao.learning.in.java.concurrency;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * CountDownLatch：在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待
+ * 注意：CountDownLatch的计数器无法被重置
+ */
+public class CountDownLatchDemo {
+
+    public static void main(String[] args) {
+        CountDownLatch countDownLatch = new CountDownLatch(8);
+        new Thread(() -> {
+            try {
+                countDownLatch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("800米比赛结束，准备清空跑道并继续跨栏比赛");
+        }).start();
+
+        for (int i = 0; i < 8; i++) {
+            int finalI = i;
+            new Thread(() -> {
+                try {
+                    Thread.sleep(finalI * 1000L);
+                    System.out.println(Thread.currentThread().getName() + "到达终点");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            }).start();
+        }
+    }
+}
