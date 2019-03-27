1a112d2b53092c80c8ec98725556e727723ad811
enhao <liangenhao@hotmail.com>
2019-03-27 20:18:05 +0800

    Add 门面模式通用写法

---
+++ design-pattern/src/main/java/com/enhao/learning/in/java/design/pattern/facade/SubsystemC.java
@@ -0,0 +1,13 @@
+package com.enhao.learning.in.java.design.pattern.facade;
+
+/**
+ * 子系统A
+ *
+ * @author enhao
+ */
+public class SubsystemC {
+
+    public void doSomethingC() {
+        System.out.println("sub system C do something C");
+    }
+}
